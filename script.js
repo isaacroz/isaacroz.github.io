216ae0c6b742f4baf6e4c133de1fad09c799eb0e
Izzat Radzi <167152025+isaacroz@users.noreply.github.com>
2025-05-16 21:18:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('nav');
-    
-    menuToggle.addEventListener('click', function() {
-        this.classList.toggle('active');
-        nav.classList.toggle('active');
-    });
-    
-    // Close menu when clicking outside
-    document.addEventListener('click', function(e) {
-        if (!e.target.closest('header')) {
-            menuToggle.classList.remove('active');
-            nav.classList.remove('active');
-        }
-    });
-    
     // Update year in footer
     const currentYear = document.getElementById('current-year');
     if (currentYear) {
